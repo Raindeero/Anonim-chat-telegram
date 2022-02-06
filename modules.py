25d8af41b37000899b538c1ee734c55d338de401
Raindeero <56960571+Raindeero@users.noreply.github.com>
2022-02-06 18:12:23 +0100

    Update modules.py

--- modules.py
+++ modules.py
@@ -7,7 +7,7 @@ from DB import SQLite_db
 from middleware import Middleware
 
 loop = asyncio.get_event_loop()
-bot = Bot(token="5233063802:AAExdafDrA_fTgyD07IsKB8e7YvdQXxB_38", loop=loop)
+bot = Bot(token="5233063802:AAHOLlnI2PTivoyTPxksZulyHqfjvxC-biE", loop=loop)
 storage = MemoryStorage()
 dp = Dispatcher(bot, loop=loop, storage=storage)
 db = SQLite_db('database.db')
